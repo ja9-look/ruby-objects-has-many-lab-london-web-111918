9e77e84041c6d77b6d5bc7e90bb3bb03e9d6f130
Janine Luk <janineluk95@gmail.com>
2018-11-23 18:52:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,28 @@
-# class Artist 
+class Artist 
   
-#   attr_accessor :name, :songs
+  attr_accessor :name, :songs
   
 
-#   def initialize(name)
-#     @name = self.name
-#     @name = name
-#     @songs = []
-#   end
+  def initialize(name)
+    @name = self.name
+    @name = name
+    @songs = []
+  end
 
-#   def add_song(song)
-#     @songs << song
-#     song.artist = self
-#   end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
   
-#   def add_song_by_name(name)
-#     song = Song.new(name)
-#     @songs << song
-#     song.artist = self
-#   end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
   
-#   def self.song_count
+  def self.song_count
     
-#   end
+  end
   
-# end
+end
 
